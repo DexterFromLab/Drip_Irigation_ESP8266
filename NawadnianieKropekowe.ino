801996561c0452e52be7dcbe2cbc4d6b8d371b97
DexterFromLab <15119912@wp.pl>
2016-06-04 19:20:25 +0200

    Pierwszy commit
    Po zmianach z próbą odbioru danych przez ajax plik się nie kompuluje

---
+++ NawadnianieKropekowe.ino
@@ -0,0 +1,469 @@
+/*
+  FSWebServer - Example WebServer with SPIFFS backend for esp8266
+  Copyright (c) 2015 Hristo Gochkov. All rights reserved.
+  This file is part of the ESP8266WebServer library for Arduino environment.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+  Yo
+  
+  
+  
+  
+  
+  
+  
+  u should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+  upload the contents of the data folder with MkSPIFFS Tool ("ESP8266 Sketch Data Upload" in Tools menu in Arduino IDE)
+  or you can upload the contents of a folder if you CD in that folder and run the following command:
+  for file in `ls -A1`; do curl -F "file=@$PWD/$file" esp8266fs.local/edit; done
+
+  access the sample web page at http://esp8266fs.local
+  edit the page by going to http://esp8266fs.local/edit
+*/
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <FS.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <TimeLib.h>
+#include <TimeAlarms.h>
+#include <WiFiUdp.h>
+
+
+#define DBG_OUTPUT_PORT Serial
+
+#define ONE_WIRE_BUS 2  // DS18B20 pin
+
+#define wielokosc_bufora_pomiarow 100   //Ilość pomiarów do wykresu
+#define POMIARY_USREDNIANE	5
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature DS18B20(&oneWire);
+//Measures
+
+float temps[wielokosc_bufora_pomiarow];
+float temps_avg[POMIARY_USREDNIANE];
+const char* ssid = "nokia";
+const char* password = "pingwin199";
+const char* host = "esp8266fs";
+
+ESP8266WebServer server(80);
+//holds the current upload
+File fsUploadFile;
+unsigned int i;//licznik petli
+unsigned char tmp_cnt;
+unsigned char buff_full_cnt;
+unsigned int temps_cnt;
+
+unsigned int measure_intervall = 173;
+
+// NTP Servers:
+static const char ntpServerName[] = "us.pool.ntp.org";
+
+const int timeZone = 2;     // Central European Time
+
+WiFiUDP Udp;
+unsigned int localPort = 8888;  // local port to listen for UDP packets
+
+float temp;               //przechowuję chwilową wartość temp
+int last_meas_time[6];
+void measure_task() {
+
+  float suma;
+  char tmpStr[150];
+  DS18B20.requestTemperatures();
+  temp = DS18B20.getTempCByIndex(0);
+  temps_avg[tmp_cnt] = temp;
+  //Serial.print("Temperature: ");
+ // Serial.println(temps_avg[tmp_cnt]);
+	snprintf(tmpStr,sizeof(tmpStr),"Temps%d: ",tmp_cnt);
+	Serial.print(tmpStr);
+	Serial.println(temps_avg[tmp_cnt]);
+	if (tmp_cnt >= (POMIARY_USREDNIANE -1)) {
+    //temps[temps_cnt];
+
+    for (i = 0; i < POMIARY_USREDNIANE ; i++) {
+      suma += temps_avg[i];
+    }
+	if(buff_full_cnt < wielokosc_bufora_pomiarow){
+		temps[buff_full_cnt] = suma / (float)POMIARY_USREDNIANE;
+		buff_full_cnt++;
+	}else{
+		for(i = 0;i<=(wielokosc_bufora_pomiarow - 1);i++){
+			temps[i] = temps[i+1];
+		}
+		temps[wielokosc_bufora_pomiarow - 1] = suma / (float)POMIARY_USREDNIANE;
+	}
+    for(i = 0;i<wielokosc_bufora_pomiarow;i++){
+		snprintf(tmpStr,sizeof(tmpStr),"Bufor pomiaru%d: ",i);
+		Serial.print(tmpStr);
+		Serial.println(temps[i]);
+	}
+    Serial.print("Srednia: ");
+    Serial.println(temps[0]);
+    if(temps_cnt < wielokosc_bufora_pomiarow) temps_cnt++;
+	
+	last_meas_time[0] = year();
+	last_meas_time[1] = month();
+	last_meas_time[2] = day();
+	last_meas_time[3] = hour();
+	last_meas_time[4] = minute();
+	last_meas_time[5] = second();
+	
+    tmp_cnt = 0;
+    return;
+  }
+
+  tmp_cnt++;
+}
+
+//format bytes
+String formatBytes(size_t bytes) {
+  if (bytes < 1024) {
+    return String(bytes) + "B";
+  } else if (bytes < (1024 * 1024)) {
+    return String(bytes / 1024.0) + "KB";
+  } else if (bytes < (1024 * 1024 * 1024)) {
+    return String(bytes / 1024.0 / 1024.0) + "MB";
+  } else {
+    return String(bytes / 1024.0 / 1024.0 / 1024.0) + "GB";
+  }
+}
+
+String getContentType(String filename) {
+  if (server.hasArg("download")) return "application/octet-stream";
+  else if (filename.endsWith(".htm")) return "text/html";
+  else if (filename.endsWith(".html")) return "text/html";
+  else if (filename.endsWith(".css")) return "text/css";
+  else if (filename.endsWith(".js")) return "application/javascript";
+  else if (filename.endsWith(".png")) return "image/png";
+  else if (filename.endsWith(".gif")) return "image/gif";
+  else if (filename.endsWith(".jpg")) return "image/jpeg";
+  else if (filename.endsWith(".ico")) return "image/x-icon";
+  else if (filename.endsWith(".xml")) return "text/xml";
+  else if (filename.endsWith(".pdf")) return "application/x-pdf";
+  else if (filename.endsWith(".zip")) return "application/x-zip";
+  else if (filename.endsWith(".gz")) return "application/x-gzip";
+  return "text/plain";
+}
+void handleControl(String path){
+	 DBG_OUTPUT_PORT.println("handleControl: " + path);
+}
+bool handleFileRead(String path) {
+  DBG_OUTPUT_PORT.println("handleFileRead: " + path);
+  if (path.endsWith("/")) path += "index.htm";
+  String contentType = getContentType(path);
+  String pathWithGz = path + ".gz";
+  if (SPIFFS.exists(pathWithGz) || SPIFFS.exists(path)) {
+    if (SPIFFS.exists(pathWithGz))
+      path += ".gz";
+    File file = SPIFFS.open(path, "r");
+    size_t sent = server.streamFile(file, contentType);
+    file.close();
+    return true;
+  }
+  return false;
+}
+
+void handleFileUpload() {
+  if (server.uri() != "/edit") return;
+  HTTPUpload& upload = server.upload();
+  if (upload.status == UPLOAD_FILE_START) {
+    String filename = upload.filename;
+    if (!filename.startsWith("/")) filename = "/" + filename;
+    DBG_OUTPUT_PORT.print("handleFileUpload Name: "); DBG_OUTPUT_PORT.println(filename);
+    fsUploadFile = SPIFFS.open(filename, "w");
+    filename = String();
+  } else if (upload.status == UPLOAD_FILE_WRITE) {
+    //DBG_OUTPUT_PORT.print("handleFileUpload Data: "); DBG_OUTPUT_PORT.println(upload.currentSize);
+    if (fsUploadFile)
+      fsUploadFile.write(upload.buf, upload.currentSize);
+  } else if (upload.status == UPLOAD_FILE_END) {
+    if (fsUploadFile)
+      fsUploadFile.close();
+    DBG_OUTPUT_PORT.print("handleFileUpload Size: "); DBG_OUTPUT_PORT.println(upload.totalSize);
+  }
+}
+
+void handleFileDelete() {
+  if (server.args() == 0) return server.send(500, "text/plain", "BAD ARGS");
+  String path = server.arg(0);
+  DBG_OUTPUT_PORT.println("handleFileDelete: " + path);
+  if (path == "/")
+    return server.send(500, "text/plain", "BAD PATH");
+  if (!SPIFFS.exists(path))
+    return server.send(404, "text/plain", "FileNotFound");
+  SPIFFS.remove(path);
+  server.send(200, "text/plain", "");
+  path = String();
+}
+
+void handleFileCreate() {
+  if (server.args() == 0)
+    return server.send(500, "text/plain", "BAD ARGS");
+  String path = server.arg(0);
+  DBG_OUTPUT_PORT.println("handleFileCreate: " + path);
+  if (path == "/")
+    return server.send(500, "text/plain", "BAD PATH");
+  if (SPIFFS.exists(path))
+    return server.send(500, "text/plain", "FILE EXISTS");
+  File file = SPIFFS.open(path, "w");
+  if (file)
+    file.close();
+  else
+    return server.send(500, "text/plain", "CREATE FAILED");
+  server.send(200, "text/plain", "");
+  path = String();
+}
+
+void handleFileList() {
+  if (!server.hasArg("dir")) {
+    server.send(500, "text/plain", "BAD ARGS");
+    return;
+  }
+
+  String path = server.arg("dir");
+  DBG_OUTPUT_PORT.println("handleFileList: " + path);
+  Dir dir = SPIFFS.openDir(path);
+  path = String();
+
+  String output = "[";
+  while (dir.next()) {
+    File entry = dir.openFile("r");
+    if (output != "[") output += ',';
+    bool isDir = false;
+    output += "{\"type\":\"";
+    output += (isDir) ? "dir" : "file";
+    output += "\",\"name\":\"";
+    output += String(entry.name()).substring(1);
+    output += "\"}";
+    entry.close();
+  }
+
+  output += "]";
+  server.send(200, "text/json", output);
+}
+
+void Repeats(){
+  Serial.println("15 second timer");         
+}
+void setup(void) {
+  DBG_OUTPUT_PORT.begin(115200);
+  DBG_OUTPUT_PORT.print("\n");
+  DBG_OUTPUT_PORT.setDebugOutput(true);
+  SPIFFS.begin();
+  {
+    Dir dir = SPIFFS.openDir("/");
+    while (dir.next()) {
+      String fileName = dir.fileName();
+      size_t fileSize = dir.fileSize();
+      DBG_OUTPUT_PORT.printf("FS File: %s, size: %s\n", fileName.c_str(), formatBytes(fileSize).c_str());
+    }
+    DBG_OUTPUT_PORT.printf("\n");
+  }
+  //mesures
+  setTime(8,29,0,1,1,11); // set time to Saturday 8:29:00am Jan 1 2011
+  Alarm.timerRepeat(measure_intervall, measure_task);
+  digitalClockDisplay();
+
+  //WIFI INIT
+  Serial.print("Configuring access point...");
+  // You can remove the password parameter if you want the AP to be open. 
+  //WiFi.softAP("NodeMcu", "12345678");
+ 
+  //  IPAddress myIP = WiFi.softAPIP();
+   // Serial.print("AP IP address: ");
+   // Serial.println(myIP);
+  //
+
+  DBG_OUTPUT_PORT.printf("Connecting to %s\n", ssid);
+  if (String(WiFi.SSID()) != String(ssid)) {
+    WiFi.begin(ssid, password);
+  }
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    DBG_OUTPUT_PORT.print(".");
+
+    DBG_OUTPUT_PORT.println("");
+    DBG_OUTPUT_PORT.print("Connected! IP address: ");
+    DBG_OUTPUT_PORT.println(WiFi.localIP());
+  }
+  MDNS.begin(host);
+  DBG_OUTPUT_PORT.print("Open http://");
+  DBG_OUTPUT_PORT.print(host);
+  DBG_OUTPUT_PORT.println(".local/edit to see the file browser");
+
+
+  //SERVER INIT
+  //Ustawienia sterowania
+  server.on("/cont", HTTP_GET, handleFileList);
+  //list directory
+  server.on("/list", HTTP_GET, handleFileList);
+  //load editor
+  server.on("/edit", HTTP_GET, []() {
+    if (!handleFileRead("/edit.htm")) server.send(404, "text/plain", "FileNotFound");
+  });
+  //create file
+  server.on("/edit", HTTP_PUT, handleFileCreate);
+  //delete file
+  server.on("/edit", HTTP_DELETE, handleFileDelete);
+  //first callback is called after the request has ended with all parsed arguments
+  //second callback handles file uploads at that location
+  server.on("/edit", HTTP_POST, []() {
+    server.send(200, "text/plain", "");
+  }, handleFileUpload);
+
+  //called when the url is not defined here
+  //use it to load content from SPIFFS
+  server.onNotFound([]() {
+    if (!handleFileRead(server.uri()))
+      server.send(404, "text/plain", "FileNotFound");
+  });
+
+  //get heap status, analog input value and all GPIO statuses in one json call
+  server.on("/all", HTTP_GET, []() {
+    
+    String json = "{";
+    json += "\"heap\":" + String(ESP.getFreeHeap());
+    json += ",\"analog\":" + String(analogRead(A0));
+    json += ",\"gpio\":" + String((uint32_t)(((GPI | GPO) & 0xFFFF) | ((GP16I & 0x01) << 16)));
+    json += ",\"meas_cnt\":" + String(temps_cnt);
+    json += ",\"temperatura\":"  + String(temp);
+    json += ",\"year\":"  + String(year());
+    json += ",\"month\":"  + String(month());
+    json += ",\"day\":"  + String(day());
+    json += ",\"second\":"  + String(second());
+    json += ",\"minute\":"  + String(minute());
+    json += ",\"hour\":"  + String(hour());
+	json += ",\"measure_intervall\":" + String(measure_intervall * POMIARY_USREDNIANE);
+	for(i=0;i<=5;i++){
+		json += ",\"last_meas_time" +String(i)+"\":" + String(last_meas_time[i]);
+	}
+    for(i = 0;i<=wielokosc_bufora_pomiarow;i++){
+        json += ",\"t" + String(i) + "\":" + String(temps[i]);
+      }
+
+    json += "}";
+    server.send(200, "text/json", json);
+    json = String();
+  });
+
+  Serial.println("Starting UDP");
+  Udp.begin(localPort);
+  Serial.print("Local port: ");
+  Serial.println(Udp.localPort());
+  Serial.println("waiting for sync");
+  setSyncProvider(getNtpTime);
+  setSyncInterval(300);
+  
+  server.begin();
+  DBG_OUTPUT_PORT.println("HTTP server started");
+}
+time_t prevDisplay = 0; // when the digital clock was displayed
+void loop(void) {
+  if (timeStatus() != timeNotSet) {
+    if (now() != prevDisplay) { //update the display only if time has changed
+      prevDisplay = now();
+      //digitalClockDisplay();
+    }
+  }
+  
+  server.handleClient();
+  Alarm.delay(1); // wait one second between clock display
+}
+
+/*-------- NTP code ----------*/
+
+const int NTP_PACKET_SIZE = 48; // NTP time is in the first 48 bytes of message
+byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
+
+time_t getNtpTime()
+{
+  IPAddress ntpServerIP; // NTP server's ip address
+
+  while (Udp.parsePacket() > 0) ; // discard any previously received packets
+  Serial.println("Transmit NTP Request");
+  // get a random server from the pool
+  WiFi.hostByName(ntpServerName, ntpServerIP);
+  Serial.print(ntpServerName);
+  Serial.print(": ");
+  Serial.println(ntpServerIP);
+  sendNTPpacket(ntpServerIP);
+  uint32_t beginWait = millis();
+  while (millis() - beginWait < 1500) {
+    int size = Udp.parsePacket();
+    if (size >= NTP_PACKET_SIZE) {
+      Serial.println("Receive NTP Response");
+      Udp.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
+      unsigned long secsSince1900;
+      // convert four bytes starting at location 40 to a long integer
+      secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
+      secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
+      secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
+      secsSince1900 |= (unsigned long)packetBuffer[43];
+      return secsSince1900 - 2208988800UL + timeZone * SECS_PER_HOUR;
+    }
+  }
+  Serial.println("No NTP Response :-(");
+  return 0; // return 0 if unable to get the time
+}
+
+// send an NTP request to the time server at the given address
+void sendNTPpacket(IPAddress &address)
+{
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12] = 49;
+  packetBuffer[13] = 0x4E;
+  packetBuffer[14] = 49;
+  packetBuffer[15] = 52;
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  Udp.beginPacket(address, 123); //NTP requests are to port 123
+  Udp.write(packetBuffer, NTP_PACKET_SIZE);
+  Udp.endPacket();
+}
+void digitalClockDisplay()
+{
+  // digital clock display of the time
+  Serial.print(hour());
+  printDigits(minute());
+  printDigits(second());
+  Serial.print(" ");
+  Serial.print(day());
+  Serial.print(".");
+  Serial.print(month());
+  Serial.print(".");
+  Serial.print(year());
+  Serial.println();
+}
+
+void printDigits(int digits)
+{
+  // utility for digital clock display: prints preceding colon and leading 0
+  Serial.print(":");
+  if (digits < 10)
+    Serial.print('0');
+  Serial.print(digits);
+}
+
+
